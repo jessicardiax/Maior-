60aa55e5996463a3fe5f77c86456bfc9ea4a5c1f
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2022-11-16 21:20:17 -0300

    Add files via upload

---
+++ main (15).c
@@ -0,0 +1,24 @@
+//Faça um Programa que leia três números e mostre o maior deles.
+
+#include <stdio.h>
+int main(){
+    int N1,N2,N3,maior;
+    printf("digite um numero:\n");
+    scanf("%d",&N1);
+    printf("digite outro numero:\n");
+    scanf("%d",&N2);
+    printf("digite outro numero:\n");
+    scanf("%d",&N3);
+    
+maior=N1;
+    if (N2>maior){
+        maior=N2;
+        
+    }
+     if (N3>maior){
+        maior=N3;
+    }
+        printf("o maior numero é:%.d\n",maior);
+    
+    return 0;
+} 
